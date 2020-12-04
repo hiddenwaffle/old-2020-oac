fbbeacb2ed9fd90452cd04a47a8e0805dd047033
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-12-04 00:28:21 -0600

    Initial attempt 04b

--- aoc/04a.py
+++ aoc/04a.py
@@ -1,6 +1,7 @@
 import re
 
 REGEX = re.compile(r'([a-z]+):([a-zA-z0-9#]+)')  # https://pythex.org/
+HGT_REGEX = re.compile(r'(\d+)(cm|in)')
 
 # NOTE: cid is optional (1/2)
 REQUIRED_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
@@ -23,6 +24,64 @@ class Passport:
         for required_field in REQUIRED_FIELDS:
             if required_field not in self.fields:
                 return False
+        # Additional validation
+        # byr
+        if len(self.fields['byr']) != 4:
+            return False
+        byr = int(self.fields['byr'])
+        if byr < 1920 or byr > 2002:
+            return False
+        # iyr
+        if len(self.fields['iyr']) != 4:
+            return False
+        iyr = int(self.fields['iyr'])
+        if iyr < 2010 or iyr > 2020:
+            return False
+        if len(self.fields['eyr']) != 4:
+            return False
+        # eyr
+        eyr = int(self.fields['eyr'])
+        if eyr < 2020 or eyr > 2030:
+            return False
+        # hgt
+        if not self.valid_hgt():
+            return False
+        # hcl
+        if not self.valid_hcl():
+            return False
+        # ecl
+        if not self.fields['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+            return False
+        # pid
+        if len(self.fields['pid']) != 9:
+            return False
+        try:
+            int(self.fields['pid'])
+        except ValueError:
+            return False
+        return True
+
+    def valid_hgt(self):
+        match = HGT_REGEX.match(self.fields['hgt'])
+        if match:
+            num = int(match.group(1))
+            measure = match.group(2)
+            if measure == 'cm' and (num < 150 or num > 193):
+                return False
+            elif measure == 'in' and (num < 59 or num > 76):
+                return False
+            elif measure not in ['cm', 'in']:
+                return False
+        return True
+
+    def valid_hcl(self):
+        if not self.fields['hcl'].startswith('#'):
+            return False
+        # https://stackoverflow.com/a/29643643
+        try:
+            tuple(int(self.fields['hcl'].lstrip('#')[i:i + 2], 16) for i in (0, 2, 4))
+        except ValueError:
+            return False
         return True
 
     def __str__(self):
