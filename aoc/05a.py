4c9848573b6c9456b4ae2277dcd7a17d41a9a24a
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-12-04 23:20:24 -0600

    05a

--- aoc/05a.py
+++ aoc/05a.py
@@ -1,13 +1,49 @@
-# import re
+def adjust_range(low, high, half):
+    mid_start = (low + high + 1) / 2
+    if half == 'lower':
+        return low, mid_start - 1
+    elif half == 'upper':
+        return mid_start, high
+    else:
+        raise Exception(half)
 
 
-# https://pythex.org/
+def bsp_to_seat(line):
+    row_low = 0
+    row_high = 127
+    for fb in line[:7]:
+        if fb == 'F':
+            row_low, row_high = adjust_range(row_low, row_high, 'lower')
+        elif fb == 'B':
+            row_low, row_high = adjust_range(row_low, row_high, 'upper')
+        else:
+            raise Exception(fb)
+    if row_low != row_high:
+        raise Exception(f'{row_low}, {row_high}')
+    seat_low = 0
+    seat_high = 7
+    for lr in line[7:10]:
+        if lr == 'L':
+            seat_low, seat_high = adjust_range(seat_low, seat_high, 'lower')
+        elif lr == 'R':
+            seat_low, seat_high = adjust_range(seat_low, seat_high, 'upper')
+        else:
+            raise Exception(lr)
+    if seat_low != seat_high:
+        raise Exception(f'{seat_low}, {seat_high}')
+    return row_low * 8 + seat_low
 
 
 def main():
-    with open('input/05_example.data') as file:
+    with open('input/05.data') as file:
         lines = file.read().splitlines()
-    print(lines)
+    max_seat = -1
+    for line in lines:
+        seat = bsp_to_seat(line)
+        print(line, '->', seat)
+        if seat > max_seat:
+            max_seat = seat
+    print('max_seat', max_seat)
 
 
 if __name__ == '__main__':
