12fc7ff6537a6860892fc0c9492c38bde208f4a6
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-12-04 22:01:47 -0600

    Add file for 05a

---
+++ aoc/05a.py
@@ -0,0 +1,14 @@
+# import re
+
+
+# https://pythex.org/
+
+
+def main():
+    with open('input/05_example.data') as file:
+        lines = file.read().splitlines()
+    print(lines)
+
+
+if __name__ == '__main__':
+    main()
