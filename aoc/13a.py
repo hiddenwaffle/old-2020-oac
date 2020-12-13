0b0033cf37bd9e2204bdbb5bf8a2bfdaea321b05
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-12-12 23:20:49 -0600

    Add 13a

--- aoc/13a.py
+++ aoc/13a.py
@@ -1,7 +1,34 @@
+from math import inf
+
+
+def to_int(x):
+    if x.isnumeric():
+        return int(x)
+    else:
+        return None
+
+
 def main():
-    with open('input/13_example.data') as file:
+    with open('input/13.data') as file:
         lines = file.read().splitlines()
-    print(lines)
+    start_time = int(lines[0])
+    buses = [bus for bus in list(map(to_int, lines[1].split(',')))
+             if bus]  # Filter out None elements
+    nearest_bus = None
+    nearest_diff = inf
+    for bus in buses:
+        x = start_time // bus
+        y = bus * (x + 1)
+        diff = y - start_time
+        if diff < nearest_diff:
+            nearest_bus = bus
+            nearest_diff = diff
+    print(nearest_bus * nearest_diff)
+
+
+def thing():
+    print('doing the thing')
+    return 10
 
 
 if __name__ == '__main__':
---
+++ aoc/13b.py
@@ -0,0 +1,24 @@
+from math import inf
+
+
+def to_int(x):
+    if x.isnumeric():
+        return int(x)
+    else:
+        return None
+
+
+def main():
+    with open('input/13.data') as file:
+        lines = file.read().splitlines()
+    buses = list(map(to_int, lines[1].split(',')))
+    print(buses)
+
+
+def thing():
+    print('doing the thing')
+    return 10
+
+
+if __name__ == '__main__':
+    main()
