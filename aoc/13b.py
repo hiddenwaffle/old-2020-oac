338fdabbf30d1319266e81a6406b4ba21ef10c24
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-12-13 12:07:07 -0600

    Add comments

--- aoc/13b.py
+++ aoc/13b.py
@@ -10,11 +10,12 @@ def main():
         lines = file.read().splitlines()
     buses = list(map(to_int, lines[1].split(',')))
     print(buses)
+
     # The earliest timestamp that matches the list 17,x,13,19 is 3417.
     #
-    # 17 - (t % 17) = 17
-    # 13 - (t % 13) = 2
-    # 19 - (t % 19) = 3
+    # 17 - (t % 17) = 17    <-- since 17 % itself is 0
+    # 13 - (t % 13) = 2     <-- where 2 is offset+2
+    # 19 - (t % 19) = 3     <-- where 3 is offset+3
     #
     # t % 17 = 0
     # t % 13 = 11
