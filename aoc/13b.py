e057f3e80f7f8869fa8b8e4c1773746f0f35ada0
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-12-13 17:17:41 -0600

    Add brute force

--- aoc/13b.py
+++ aoc/13b.py
@@ -6,11 +6,41 @@ def to_int(x):
 
 
 def main():
-    with open('input/13_example.data') as file:
+    with open('input/13.data') as file:
         lines = file.read().splitlines()
     buses = list(map(to_int, lines[1].split(',')))
-    print(buses)
+    equations = []
+    for i, bus in enumerate(buses):
+        if bus:
+            equations.append(f'mod[t, {bus}] = {i}')
+    print(', '.join(equations))  # Paste into Wolfram Alpha
+    t = buses[0]
+    pindex = 0
+    remainders = [0] * len(buses)
+    for i, bus in enumerate(buses):
+        if bus:
+            remainders[i] = (bus - i) % buses[i]
+        else:
+            remainders[i] = None
+    while True:
+        success = True
+        for i, bus in enumerate(buses):
+            if bus:
+                if t % bus != remainders[i]:
+                    success = False
+                    break
+                else:
+                    pass
+        if success:
+            print('Success:', t)
+            break
+        t += buses[0]
+        pindex += 1
+        if pindex % 90000000 == 0:
+            print('t', t)
+            pindex = 0
 
+    # OLD NOTES:
     # The earliest timestamp that matches the list 17,x,13,19 is 3417.
     #
     # 17 - (t % 17) = 17    <-- since 17 % itself is 0
