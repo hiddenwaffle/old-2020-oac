8b25ec183b0ef9b9762b2af660d6e5f19a76be4a
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2020-12-13 10:18:41 -0600

    Add note for 13b

--- aoc/13b.py
+++ aoc/13b.py
@@ -1,6 +1,3 @@
-from math import inf
-
-
 def to_int(x):
     if x.isnumeric():
         return int(x)
@@ -13,11 +10,17 @@ def main():
         lines = file.read().splitlines()
     buses = list(map(to_int, lines[1].split(',')))
     print(buses)
-
-
-def thing():
-    print('doing the thing')
-    return 10
+    # The earliest timestamp that matches the list 17,x,13,19 is 3417.
+    #
+    # 17 - (t % 17) = 17
+    # 13 - (t % 13) = 2
+    # 19 - (t % 19) = 3
+    #
+    # t % 17 = 0
+    # t % 13 = 11
+    # t % 19 = 16
+    #
+    # TODO: Use the Extended Euclidean algorithm to find t?
 
 
 if __name__ == '__main__':
